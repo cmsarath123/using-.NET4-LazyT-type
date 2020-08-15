fc900fe44364bdf10dc086cd5c1e6306a7a13398
sarathkumarc <cmsarath123@gmail.com>
2020-08-15 21:28:04 +0530

    singleton demo

---
+++ singletondemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace singletondemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton _singletonObj = Singleton.GetInstance;
+            _singletonObj.WriteMessage("hello world");
+            Singleton _anotherObj = Singleton.GetInstance;
+            _anotherObj.WriteMessage("how are you");
+        }
+    }
+}
---
+++ singletondemo/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singletondemo
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        private static int ObjectsCount = 0;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+
+        private Singleton()
+        {
+            ObjectsCount++;
+            Console.WriteLine($"instances {ObjectsCount}");
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
